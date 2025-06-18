d5a3a8b7a8588cd3dc3a8f715789a7729d87a040
mockingjae <macqueen01@berkeley.edu>
2025-06-18 11:49:26 +0900

    feat: add Point3ManagerRoleType, ManagerRoleType enum

---
+++ values/roles.ts
@@ -0,0 +1,21 @@
+export enum ManagerRoleType {
+    ADMIN = 'admin-manager',
+    MANAGEMENT = 'management-manager',
+    CHECKOUT = 'checkout-manager',
+    DEVELOPER = 'developer-manager',
+    DEFAULT = 'default-manager',
+
+    // Point3 매니저 역할 (임시)
+    POINT3_ADMIN = 'p3-CISO-0',
+    POINT3_DEVELOPER = 'p3-DEV-0',
+    POINT3_INFRA = 'p3-INFRA-0',
+    POINT3_SECURITY = 'p3-SECURITY-0',
+}
+
+export enum Point3ManagerRoleType {
+    POINT3_ADMIN = 'p3-CISO-0',
+    POINT3_DEVELOPER = 'p3-DEV-0',
+    POINT3_INFRA = 'p3-INFRA-0',
+    POINT3_SECURITY = 'p3-SECURITY-0',
+}
+
